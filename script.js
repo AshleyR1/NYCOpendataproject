0e33b01a8a13f2fdc5592138fbdd9c09e6e78f9f
AshleyR1 <ashleyr698@nycstudents.net>
2025-05-19 17:48:40 +0000

    chart not work

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
 //jsondata  link:https://data.cityofnewyork.us/resource/jb7j-dtam.json
-     var textWrapper = document.querySelector('.ani .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-ani.timeline({loop: true})
-  .add({
-    targets: '.ani .letter',
-    scale: [0.3,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 600,
-    delay: (el, i) => 70 * (i+1)
-  }).add({
-    targets: '.ani .line',
-    scaleX: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
-    duration: 700,
-    offset: '-=875',
-    delay: (el, i, l) => 80 * (l - i)
-  }).add({
-    targets: '.ani',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
 
 
